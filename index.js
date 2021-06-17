bc77a26e4cd9111b1813ed440d95ce95a7cedd86
mflem8 <matthewfleming8@gmail.com>
2021-06-17 19:25:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const text = document.getElementById("text");
+
+const changeP = () => {
+    text.innerHTML = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeP();
+});
